69aad84c11f3bbf2f41fad6f2f1ac54c13576ffd
m.karandish <maryam.karandish4@gmail.com>
2018-03-15 10:39:00 +0330

    Initialize

---
+++ src/com/toy/Program.java
@@ -0,0 +1,10 @@
+package com.toy;
+
+/**
+ * Created by m.karandish on 3/15/18.
+ */
+public class Program {
+    public static void main(String[] args) {
+        System.out.println("hi!!!!!!!!!!");
+    }
+}
