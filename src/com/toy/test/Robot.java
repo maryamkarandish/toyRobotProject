1f8a44e368fa615a2f6d2611c50589a6d51b73b8
m.karandish <maryam.karandish4@gmail.com>
2018-03-15 10:36:35 +0330

    Initialize project

---
+++ src/com/toy/test/Robot.java
@@ -0,0 +1,37 @@
+package com.toy.test;
+
+/**
+ * Created by m.karandish on 3/15/18.
+ */
+public class Robot {
+
+
+    private int x;
+    private int y;
+
+    public Robot(int x,int y){
+
+    }
+
+    public Robot(){
+
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+}
+
+
